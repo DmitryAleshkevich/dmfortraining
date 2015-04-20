88e87dd3792a8fcb6ffb8e5f7a59c23524a84e7b
DmitryAleshkevich <dmitryaleshkevich@gmail.com>
2015-04-20 14:49:58 +0300

    This is problem1 solution for courses

---
+++ ConsoleProblem1/LinearFunction.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleProblem1
+{
+    public class LinearFunction
+    {
+        #region Fields
+
+        private double coeffA { get; set; }
+
+        private double coeffB { get; set; }
+
+        #endregion
+
+        #region Methods
+
+        public double GetFunctionValue(double variableX)
+        {
+            return this.coeffA * variableX + this.coeffB;
+        }
+
+        #endregion
+
+        #region Constructors
+
+        public LinearFunction() { }
+       
+        public LinearFunction(double coeffA, double coeffB)
+        {
+            this.coeffA = coeffA;
+            this.coeffB = coeffB;
+        }
+
+        #endregion
+
+    }
+}
---
+++ ConsoleProblem1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleProblem1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input variable X:");
+
+            string userX = Console.ReadLine();
+
+            double x;
+
+            bool isSuccessful = double.TryParse(userX, out x);
+
+            if (isSuccessful)
+            {
+                LinearFunction lf = new LinearFunction(3, 5);
+                Console.WriteLine("Result is: {0} with coeffA = 3 & coeffB = 5 & variable {1}", lf.GetFunctionValue(x), x);
+            }
+            else Console.WriteLine("Coud not convert you x into double!");
+
+            Console.ReadLine();
+
+        }
+    }
+}
