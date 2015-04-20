4cbeebc9b3ee6c3d2729fb4cdeb8c1b4b29941c9
DmitryAleshkevich <dmitryaleshkevich@gmail.com>
2015-04-20 16:27:26 +0300

    This is a solution for problem 2 - items

---
+++ ConsoleProblem2/Item.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleProblem2
+{
+    public class Item
+    {
+        #region Fields
+
+        private string itemName;
+        private double price;
+        private double quantity;
+
+        #endregion
+
+        #region Properties
+
+        public double Cost
+        {
+            get { return this.price * this.quantity; }
+        }
+
+        public string ItemName
+        {
+            get { return itemName; }
+            set { itemName = value; }
+        }
+
+        public double Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+
+        public double Quantity
+        {
+            get { return quantity; }
+            set { quantity = value; }
+        }
+
+        #endregion
+
+        #region Constructors
+
+        public Item() { }
+
+        public Item(string itemName, double price, double quantity)
+        {
+            this.itemName = itemName;
+            this.price = price;
+            this.quantity = quantity;
+        }
+
+        #endregion
+    }
+
+    public struct StrItem
+    {
+        #region Fields
+
+        private string itemName;
+        private double price;
+        private double quantity;
+
+        #endregion
+
+        #region Properties
+
+        public double Cost
+        {
+            get { return this.price * this.quantity; }
+        }
+
+        public string ItemName
+        {
+            get { return itemName; }
+            set { itemName = value; }
+        }
+
+        public double Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+
+        public double Quantity
+        {
+            get { return quantity; }
+            set { quantity = value; }
+        }
+
+        #endregion
+
+        #region Constructors
+
+        public StrItem(string itemName, double price, double quantity)
+        {
+            this.itemName = itemName;
+            this.price = price;
+            this.quantity = quantity;
+        }
+
+        #endregion
+    }
+}
---
+++ ConsoleProblem2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleProblem2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            WorkWith(MyEnumModules.Class);
+            WorkWith(MyEnumModules.Structure);
+            Console.ReadLine();
+        }
+
+        static void WorkWith(MyEnumModules enm)
+        {
+            DateTime dt1 = DateTime.UtcNow;
+
+            ArrayList al = new ArrayList();
+            Random rand = new Random();
+            double costSum = 0;
+
+            if (enm == MyEnumModules.Class)  
+            {
+                for (int i = 1; i < 100001; i++)
+                {
+                    Item it = new Item("Item" + i, rand.Next(), rand.Next());
+                    al.Add(it);
+                    costSum += it.Cost;
+                }
+            }        
+            else
+            {
+                for (int i = 1; i < 100001; i++)
+                {
+                    StrItem it = new StrItem("Item" + i, rand.Next(), rand.Next());
+                    al.Add(it);
+                    costSum += it.Cost;
+                }
+            }
+                
+            Console.WriteLine("Total sum is {0}", costSum);
+
+            DateTime dt2 = DateTime.UtcNow;
+
+            TimeSpan dt3 = dt2 - dt1;
+
+            Console.WriteLine("Total time is {0} ms", dt3.Milliseconds);
+            
+        }
+
+        public enum MyEnumModules
+        {
+            Class,Structure
+        }
+    }
+}
