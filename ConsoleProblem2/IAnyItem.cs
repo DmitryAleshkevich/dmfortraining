29b9b303d97b5c04defe884e320b3027bb460eb5
DmitryAleshkevich <dmitryaleshkevich@gmail.com>
2015-04-21 13:35:08 +0300

    added interface

---
+++ ConsoleProblem2/IAnyItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleProblem2
+{
+    public interface IAnyItem
+    {
+        double Cost { get; }
+        IAnyItem CreateItem(string name, double price, double quantity);
+    }
+}
